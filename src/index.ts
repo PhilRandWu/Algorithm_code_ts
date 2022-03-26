a2283e80b6313aac9bcbf3a1187b04d0f5384b28
PhilRandWu <3378814052@qq.com>
2022-03-26 01:23:52 -1200

    二叉树的比较(可互换)

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-03-25 21:53:35
- * @LastEditTime: 2022-03-25 22:37:18
+ * @LastEditTime: 2022-03-26 01:22:45
  * @LastEditors: PhilRandWu
  */
 class Node {
@@ -23,13 +23,28 @@ A.right = C;
 B.left = D;
 B.right = E;
 
-function BeforeOrder(root: Node) {
-    if(root === null) {
-        return;
-    }
-    console.log(root.value);
-    BeforeOrder(root.left);
-    BeforeOrder(root.right);
+const A1 = new Node("A");
+const B1 = new Node("B");
+const C1 = new Node("C");
+const D1 = new Node("D");
+const E1 = new Node("E");
+
+A1.left = B1;
+A1.right = C1;
+B1.left = D1;
+B1.right = E1;
+
+function compare(root1: Node, root2: Node): Boolean {
+  if(root1 === root2) {
+    return true;
+  }
+  if(root1 !== null && root2 === null || root1 === null && root2 !== null) {
+    return false;
+  }
+  if(root1.value === root2.value) {
+    return true;
+  }
+  return compare(root1.left, root2.left) && compare(root1.right,root2.right) || compare(root1.left, root2.right) && compare(root1.right,root2.left);
 }
 
-BeforeOrder(A);
+console.log(compare(A,A1));
