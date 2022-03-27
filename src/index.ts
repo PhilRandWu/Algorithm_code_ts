945f5ab73d25b7a9da92797b79342d86ba69f992
PhilRandWu <3378814052@qq.com>
2022-03-27 17:22:49 +0800

    二叉树的单旋(代码实现)

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-03-25 21:53:35
- * @LastEditTime: 2022-03-27 16:51:44
+ * @LastEditTime: 2022-03-27 17:20:44
  * @LastEditors: PhilRandWu
  */
 class Node {
@@ -16,43 +16,77 @@ const A1 = new Node("A");
 const B1 = new Node("B");
 const C1 = new Node("C");
 const D1 = new Node("D");
-const E1 = new Node("E");
-const F1 = new Node("F");
-const G1 = new Node("G");
 
 A1.left = B1;
-A1.right = C1;
-B1.left = D1;
-B1.right = E1;
-C1.left = F1;
-C1.right = G1;
+B1.left = C1;
+B1.right = D1;
 
 /**
  * @description: 得到二叉树的深度
  * @param {Node} root
  * @return {*}
  */
-function getDeep(root:Node):number {
-  if(root === null) {
+function getDeep(root: Node): number {
+  if (root === null) {
     return 0;
   }
   const leftDeep = getDeep(root.left);
   const rightDeep = getDeep(root.right);
-  return Math.max(leftDeep,rightDeep) + 1;
+  return Math.max(leftDeep, rightDeep) + 1;
 }
 
-
 function isBalance(root: Node): Boolean {
-  if(root === null) {
+  if (root === null) {
     return true;
   }
   const leftDeep = getDeep(root.left);
   const rightDeep = getDeep(root.right);
-  if(Math.abs(leftDeep - rightDeep) > 1) {
+  if (Math.abs(leftDeep - rightDeep) > 1) {
     return false;
   } else {
     return isBalance(root.left) && isBalance(root.right);
   }
 }
 
+function leftRotate(root: Node): Node {
+  const newRoot = root.right;
+  const changeBranch = root.right.left;
+  root.right = changeBranch;
+  newRoot.left = root;
+  return newRoot;
+}
+
+function rightRotate(root: Node): Node {
+  const newRoot = root.left;
+  const changeBranch = root.left.right;
+  root.left = changeBranch;
+  newRoot.right = root;
+  return newRoot;
+}
+
+function change(root: Node): Node {
+  if (isBalance(root)) {
+    return root;
+  }
+  if (root.left !== null) {
+    root.left = change(root.left);
+  }
+  if (root.right !== null) {
+    root.right = change(root.right);
+  }
+  const leftDeep = getDeep(root.left);
+  const rightDeep = getDeep(root.right);
+  if (Math.abs(leftDeep - rightDeep) < 2) {
+    return root;
+  } else if (leftDeep > rightDeep) {
+    return rightRotate(root);
+  } else {
+    return leftRotate(root);
+  }
+}
+
 console.log(isBalance(A1));
+const newRoot = change(A1);
+console.log(isBalance(newRoot));
+console.log(newRoot);
+
