390640a2ce67d62920503a2d11da1b2b0d58e41c
PhilRandWu <3378814052@qq.com>
2022-03-27 16:52:05 +0800

    判断是否为平衡二叉树

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-03-25 21:53:35
- * @LastEditTime: 2022-03-27 16:33:14
+ * @LastEditTime: 2022-03-27 16:51:44
  * @LastEditors: PhilRandWu
  */
 class Node {
@@ -12,13 +12,13 @@ class Node {
   constructor(public value: string | number) {}
 }
 
-const A1 = new Node('A');
-const B1 = new Node('B');
-const C1 = new Node('C');
-const D1 = new Node('D');
-const E1 = new Node('E');
-const F1 = new Node('F');
-const G1 = new Node('G');
+const A1 = new Node("A");
+const B1 = new Node("B");
+const C1 = new Node("C");
+const D1 = new Node("D");
+const E1 = new Node("E");
+const F1 = new Node("F");
+const G1 = new Node("G");
 
 A1.left = B1;
 A1.right = C1;
@@ -27,20 +27,32 @@ B1.right = E1;
 C1.left = F1;
 C1.right = G1;
 
-function wideSearch(rootArr:Node[], target:string): boolean {
-    if(rootArr === null || rootArr.length === 0 || target === null) {
-      return false;
-    }
-    const childList: Node[] = [];
-    for(let i = 0; i < rootArr.length; i ++) {
-      if(rootArr[i] && rootArr[i].value === target) {
-        return true;
-      } else {
-        rootArr[i].left && childList.push(rootArr[i].left);
-        rootArr[i].right && childList.push(rootArr[i].right);
-      }
-    }
-    return wideSearch(childList,target);
+/**
+ * @description: 得到二叉树的深度
+ * @param {Node} root
+ * @return {*}
+ */
+function getDeep(root:Node):number {
+  if(root === null) {
+    return 0;
+  }
+  const leftDeep = getDeep(root.left);
+  const rightDeep = getDeep(root.right);
+  return Math.max(leftDeep,rightDeep) + 1;
 }
 
-console.log(wideSearch([A1],'C'));
+
+function isBalance(root: Node): Boolean {
+  if(root === null) {
+    return true;
+  }
+  const leftDeep = getDeep(root.left);
+  const rightDeep = getDeep(root.right);
+  if(Math.abs(leftDeep - rightDeep) > 1) {
+    return false;
+  } else {
+    return isBalance(root.left) && isBalance(root.right);
+  }
+}
+
+console.log(isBalance(A1));
