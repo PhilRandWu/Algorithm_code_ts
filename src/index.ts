ed5407293e6281a2e4b9a5b0c9daf91235e74db2
PhilRandWu <3378814052@qq.com>
2022-03-28 14:47:42 +0800

    图的广度优先搜素

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-03-25 21:53:35
- * @LastEditTime: 2022-03-28 14:37:46
+ * @LastEditTime: 2022-03-28 14:46:14
  * @LastEditors: PhilRandWu
  */
 class Node {
@@ -11,13 +11,13 @@ class Node {
   constructor(public value: string | number) {}
 }
 
-const A1 = new Node('A');
-const B1 = new Node('B');
-const C1 = new Node('C');
-const D1 = new Node('D');
-const E1 = new Node('E');
-const F1 = new Node('F');
-const G1 = new Node('G');
+const A1 = new Node("A");
+const B1 = new Node("B");
+const C1 = new Node("C");
+const D1 = new Node("D");
+const E1 = new Node("E");
+const F1 = new Node("F");
+const G1 = new Node("G");
 
 A1.Neighbor.push(B1);
 A1.Neighbor.push(C1);
@@ -32,24 +32,22 @@ E1.Neighbor.push(A1);
 E1.Neighbor.push(B1);
 E1.Neighbor.push(C1);
 
-function deepSearch(root:Node,target: string,path: Node[]): boolean {
-  if(root === null || !target) {
+function wideSearch(rootArr: Node[], target: string, path: Node[]): boolean {
+  if(rootArr === null || rootArr.length === 0 || !target) {
     return false;
   }
-  if(path.indexOf(root) > -1) {
-    return false;
+  let nextNodes:Node[] = [];
+  for(let i = 0; i < rootArr.length; i ++) {
+    path.push(rootArr[i]);
+    if(rootArr[i].value === target) {
+      return true;
+    } else {
+      nextNodes = nextNodes.concat(rootArr[i].Neighbor);
+    }
   }
-  if(root.value === target) {
-    return true;
-  }
-  let result = false;
-  path.push(root);
-  for(let i = 0; i < root.Neighbor.length; i ++) {
-    result ||= deepSearch(root.Neighbor[i],target,path);
-  }
-  return result ? true : false;
+  return wideSearch(nextNodes,target,path);
 }
 
 const path = [];
-deepSearch(A1, 'D',path);
+wideSearch([A1],'E',path);
 console.log(path);
