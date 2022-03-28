7710b5c3e702b932c9d37fabfabf645ba850a181
PhilRandWu <3378814052@qq.com>
2022-03-28 14:55:30 +0800

    动态规划之青蛙跳台阶问题

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,30 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-03-25 21:53:35
- * @LastEditTime: 2022-03-28 14:50:23
+ * @LastEditTime: 2022-03-28 14:54:58
  * @LastEditors: PhilRandWu
  */
 
-function Fibonacci(num: number): number {
-  if (num <= 0) return -1;
-  if (num === 1) {
+function jump(num:number): number {
+  if(num <= 0) {
+    return -1;
+  }
+  if(num === 1) {
     return 1;
   }
-  if (num === 2) {
-    return 1;
+  if(num === 2) {
+    return 2; //1 1   2
   }
-  return Fibonacci(num - 1) + Fibonacci(num - 2);
+  return jump(num - 1) + jump(num - 2);
 }
 
-console.log(Fibonacci(4));
+// 1 1 1
+// 1 2
+// 2 1
+console.log(jump(3));
+// 1 1 1 1
+// 1 2 1
+// 2 2
+// 2 1 1
+// 1 1 2
+console.log(jump(4));
