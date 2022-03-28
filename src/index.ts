878c417587e0422f24b428a5002b1581ff852cb6
PhilRandWu <3378814052@qq.com>
2022-03-28 14:51:17 +0800

    动态规划算法之斐波那契数列

--- src/index.ts
+++ src/index.ts
@@ -3,51 +3,19 @@
  * @Author: PhilRandWu
  * @Github: https://github/PhilRandWu
  * @Date: 2022-03-25 21:53:35
- * @LastEditTime: 2022-03-28 14:46:14
+ * @LastEditTime: 2022-03-28 14:50:23
  * @LastEditors: PhilRandWu
  */
-class Node {
-  public Neighbor: Node[] = [];
-  constructor(public value: string | number) {}
+
+function Fibonacci(num: number): number {
+  if (num <= 0) return -1;
+  if (num === 1) {
+    return 1;
+  }
+  if (num === 2) {
+    return 1;
+  }
+  return Fibonacci(num - 1) + Fibonacci(num - 2);
 }
 
-const A1 = new Node("A");
-const B1 = new Node("B");
-const C1 = new Node("C");
-const D1 = new Node("D");
-const E1 = new Node("E");
-const F1 = new Node("F");
-const G1 = new Node("G");
-
-A1.Neighbor.push(B1);
-A1.Neighbor.push(C1);
-A1.Neighbor.push(E1);
-B1.Neighbor.push(A1);
-B1.Neighbor.push(E1);
-C1.Neighbor.push(A1);
-C1.Neighbor.push(D1);
-C1.Neighbor.push(E1);
-D1.Neighbor.push(C1);
-E1.Neighbor.push(A1);
-E1.Neighbor.push(B1);
-E1.Neighbor.push(C1);
-
-function wideSearch(rootArr: Node[], target: string, path: Node[]): boolean {
-  if(rootArr === null || rootArr.length === 0 || !target) {
-    return false;
-  }
-  let nextNodes:Node[] = [];
-  for(let i = 0; i < rootArr.length; i ++) {
-    path.push(rootArr[i]);
-    if(rootArr[i].value === target) {
-      return true;
-    } else {
-      nextNodes = nextNodes.concat(rootArr[i].Neighbor);
-    }
-  }
-  return wideSearch(nextNodes,target,path);
-}
-
-const path = [];
-wideSearch([A1],'E',path);
-console.log(path);
+console.log(Fibonacci(4));
